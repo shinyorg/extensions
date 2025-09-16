dceef7c39b585e6e9b9be8ed088ac895361cb6e6
Allan Ritchie <allan.ritchie@gmail.com>
2025-09-16 13:32:32 -0400

    Minor

--- src/Shiny.Extensions.Device/Platforms/App/AppCultureInfo.cs
+++ src/Shiny.Extensions.Device/Platforms/App/AppCultureInfo.cs
@@ -7,4 +7,18 @@ public class AppCultureInfo : ICultureInfo
 {
     public CultureInfo Current { get; }
     public event EventHandler<CultureInfo>? Changed;
+
+    void Startup()
+    {
+#if IOS || MACCATALYST
+        var code = Foundation.NSLocale.CurrentLocale.LanguageCode;
+#elif ANDROID
+        var code = Application.Context.Resources.Configuration.Locale.Variant;
+        // Java.Util.Locale.Default.Language;
+#else
+        //CultureInfo.CurrentCulture = CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+        //CultureInfo.CurrentUICulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+#endif
+        // CultureInfo.CurrentUICulture.NumberFormat.CurrencyDecimalDigits
+    }
 }
