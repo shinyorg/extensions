c7c3edb5bf7d1111e8e5784372a280c0d70ff0c2
Allan Ritchie <allan.ritchie@gmail.com>
2025-10-14 22:26:36 -0400

    Update IDeviceOrientation.cs

--- src/Shiny.Extensions.Device/IDeviceOrientation.cs
+++ src/Shiny.Extensions.Device/IDeviceOrientation.cs
@@ -2,5 +2,24 @@ namespace Shiny.Extensions.Device;
 
 public class IDeviceOrientation
 {
-    
+    /*
+const isPortrait = window.matchMedia("(orientation: portrait)").matches;
+       
+       if (isPortrait) {
+         console.log("Device is in portrait mode.");
+       } else {
+         console.log("Device is in landscape mode.");
+       }
+       
+       // Listen for orientation changes
+       window.matchMedia("(orientation: portrait)").addEventListener("change", (e) => {
+         if (e.matches) {
+           console.log("Orientation changed to portrait.");
+           // Perform actions specific to portrait mode
+         } else {
+           console.log("Orientation changed to landscape.");
+           // Perform actions specific to landscape mode
+         }
+       });
+     */
 }
