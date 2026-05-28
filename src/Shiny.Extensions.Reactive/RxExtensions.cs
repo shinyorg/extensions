9c206ddadb5af0b6c5d1a1f3bf191d483b8bf58a
Allan Ritchie <allan.ritchie@gmail.com>
2026-05-27 23:13:28 -0400

    Add IObservable.ToTask extension; bump version
    Introduce ToTask<T> in ObservableExtensions to convert an IObservable<T> into a Task<T> that completes on the first value or error. The implementation returns a TaskCompletionSource, subscribes to the observable, supports an optional CancellationToken (disposing the subscription and cancelling the task when requested), and disposes the subscription on error. Also update version.json to move from the beta tag (4.0.0-beta.{height}) to the stable 4.0.0 release.

--- src/Shiny.Extensions.Reactive/RxExtensions.cs
+++ src/Shiny.Extensions.Reactive/RxExtensions.cs
@@ -230,4 +230,33 @@ public static class ObservableExtensions
             .Select(x => Observable.FromAsync(() => onNextAsync(x)))
             .Merge(maxConcurrent)
             .Subscribe();
+    
+    /// <summary>
+    /// Converts an IObservable to a Task, completing on the first value or error
+    /// </summary>
+    public static Task<T> ToTask<T>(this IObservable<T> observable, CancellationToken cancellationToken = default)
+    {
+        var tcs = new TaskCompletionSource<T>();
+        IDisposable? sub = null;
+
+        if (cancellationToken.CanBeCanceled)
+        {
+            cancellationToken.Register(() =>
+            {
+                sub?.Dispose();
+                tcs.TrySetCanceled();
+            });
+        }
+        sub = observable.Subscribe(
+            value => tcs.TrySetResult(value),
+            error =>
+            {
+                sub?.Dispose();
+                tcs.TrySetException(error);
+            },
+            () => { }
+        );
+
+        return tcs.Task;
+    }
 }
